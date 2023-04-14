b19d21318a24cb7bf7779899bad26ea4f791c8ee
GabrielArsenio <gabrielarsenioo@gmail.com>
2023-04-14 19:39:09 -0300

    Add while true sleep 60

--- check-internet.sh
+++ check-internet.sh
@@ -13,30 +13,33 @@ log() {
   echo "[$(date)] $1" >> $LogFile 
 }
 
-if nc -zw1 google.com 443; then
-  log "internet ON"
-else
-  log "internet OFF :("
+while true; do
+  sleep 60
+  if nc -zw1 google.com 443; then
+    log "internet ON"
+  else
+    log "internet OFF :("
 
-  shouldTurnOff=true
+    shouldTurnOff=true
 
-  for i in $(seq 1 10); do
-    sleep 1
-    log "Checking again $i"
+    for i in $(seq 1 10); do
+      sleep 1
+      log "Checking again $i"
 
-    if nc -zw1 google.com 443; then
-      log "internet ON"
-      shouldTurnOff=false
-      break
-    else
-      shouldTurnOff=true
+      if nc -zw1 google.com 443; then
+        log "internet ON"
+        shouldTurnOff=false
+        break
+      else
+        shouldTurnOff=true
+      fi
+    done
+      
+    if [[ $shouldTurnOff == true ]]; then
+      log "Turning off Umbrel"
+      log $(/home/umbrel/umbrel/scripts/stop)
+      log "Turning off Rasp"
+      log $(sudo shutdown)
     fi
-  done
-    
-  if [[ $shouldTurnOff == true ]]; then
-    log "Turning off Umbrel"
-    log $(/home/umbrel/umbrel/scripts/stop)
-    log "Turning off Rasp"
-    log $(shutdown)
   fi
-fi
+done
